23a519edbc20e3f0cd0746cf606ac745534eaa5d
Kavipriyasuresh <157814649+Kavipriyasuresh@users.noreply.github.com>
2025-03-18 15:16:42 +0530

    Add files via upload

---
+++ session1/Datatypes.java
@@ -0,0 +1,18 @@
+package programs;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		String name = "kavipriya";
+		int age = 19;
+		char initial = 's';
+		float f = 12.4567f;
+		double d = 23.456734;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(initial);
+		System.out.println(f);
+		System.out.println(d);
+	}
+
+}
---
+++ session1/Switchex.java
@@ -0,0 +1,57 @@
+package programs;
+
+import java.util.Scanner;
+
+public class Switchex {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("select an operation");
+		
+		System.out.println("1.addition");
+		System.out.println("2.subtraction");
+		System.out.println("3.multiplication");
+		System.out.println("4.division");
+		
+		System.out.println("enter the choice (1/2/3/4):");
+		int choice =sc.nextInt();
+		
+		System.out.println("enter first number:");
+		double num1 = sc.nextDouble();
+		
+		System.out.println("enter the second number:");
+		double num2 =sc.nextDouble();
+		
+		double result;
+		
+		switch(choice) {
+		case 1:
+			result = num1 + num2;
+			System.out.println("result:" +result);
+			break;
+			
+		case 2:
+			result = num1 - num2;
+			System.out.println("result:" +result);
+			break;
+			
+		case 3:
+			result = num1 * num2;
+			System.out.println("result:" +result);
+			break;
+			
+		case 4:
+			if(num2 !=0) {
+			result = num1 / num2;
+			System.out.println("result:" +result);
+			}else {
+				System.out.println("error:division by zero not allowed");
+			}
+			break;
+	default:
+	     System.out.println("invalid choice select the correct choice");
+			
+		}
+	}
+
+}
